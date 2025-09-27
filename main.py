2a206faa59f27a59ec58f0441319d080c07d6dec
AnandSagar1901 <78773233+AnandSagar1901@users.noreply.github.com>
2025-09-27 10:53:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 from google import genai
 
-API_KEY = "AIzaSyBub6LZewuE6Sya8aprw7hNKlr3k5325FU"
+API_KEY = "Your_API_key_here"
 client = genai.Client(api_key=API_KEY)
 
 def get_gemini_roast(client):
